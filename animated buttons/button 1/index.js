0aa1538858c613a1b6b161999de8e2371c0a138c
Andres Camilo Rios <acamilorm4556@gmail.com>
2023-08-19 17:30:21 -0500

    buttons 1 code actualized

--- animated buttons/button 1/index.js
+++ animated buttons/button 1/index.js
@@ -1,6 +1,20 @@
 let btn = document.querySelectorAll(".btn").forEach(btn => {
 
     btn.addEventListener("mousemove", (e) =>{
+
+        //Animación del color
+        //Color animation
+        
+        let moveX = e.pageX - btn.offsetLeft;
+        let moveY = e.pageY - btn.offsetTop;
+        
+        btn.style.setProperty("--x", moveX + "px")
+        btn.style.setProperty("--y", moveY + "px")
+        
+
+        //movimiento del botón
+        //button movement
+
         let x = e.offsetX; // almacena coordenadas x
         let y = e.offsetY; // almacena coordenadas y
         let botonWidth = btn.clientWidth; // ancho del botón
@@ -17,15 +31,8 @@ let btn = document.querySelectorAll(".btn").forEach(btn => {
             rotation = 5
         }
         btn.style.transform = `translateX(${transX}px) translateY(${transY}px) rotate(${rotation}deg)`
-
-        let mx = e.pageX - btn.offsetLeft;
-        let my = e.pageY - btn.offsetTop;
-
-        btn.style.setProperty("--x", mx + "px")
-        btn.style.setProperty("--y", my + "px")
-        
     })
-    btn.addEventListener("mouseout", (e) => {
+    btn.addEventListener("mouseout", () => {
         btn.style.transform = ""
     })
 
